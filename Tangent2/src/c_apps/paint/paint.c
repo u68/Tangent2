a5e110edd384e867b26982a6f7c5806468a5a294
u68 <78559138+u68@users.noreply.github.com>
2026-02-21 23:16:08 +1300

    forgor save

--- Tangent2/src/c_apps/paint/paint.c
+++ Tangent2/src/c_apps/paint/paint.c
@@ -1,7 +1,9 @@
 /*
- * fs_man.c
- * Implementation of file system manager
- * A simple file system manager for Tangent, you can do stuff with it.
+ * paint.c
+ * Implementation of paint application
+ * A simple paint application for Tangent, you can do stuff with it.
  *  Created on: Feb 21, 2026
  *      Author: harma
  */
+
+#include "paint.h"
--- Tangent2/src/c_apps/text_editor/text_edit.c
+++ Tangent2/src/c_apps/text_editor/text_edit.c
@@ -1,7 +1,9 @@
 /*
- * fs_man.c
- * Implementation of file system manager
- * A simple file system manager for Tangent, you can do stuff with it.
+ * text_edit.c
+ * Implementation of text editor application
+ * A simple text editor for Tangent, you can do stuff with it.
  *  Created on: Feb 21, 2026
  *      Author: harma
  */
+
+#include "text_edit.h"
