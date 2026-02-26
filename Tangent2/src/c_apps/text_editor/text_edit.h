06aa17c7ba137ca7fc4aa982c1b1c33e67680c1c
u68 <78559138+u68@users.noreply.github.com>
2026-02-26 19:24:33 +1300

    3

--- Tangent2/src/c_apps/text_editor/text_edit.h
+++ Tangent2/src/c_apps/text_editor/text_edit.h
@@ -10,6 +10,28 @@
 #define TEXT_EDIT_H_
 
 #include "../../libcw.h"
+#include "../../input/input.h"
+
+#define TE_DEFAULT_BUF 256
+#define TE_CURSOR "|"
+#define TE_DEFAULT_FONT TE_FONT_7x10
+#define TE_DEFAULT_COLOUR TUI_COLOUR_BLACK
+
+enum te_sentinel {
+    TE_TEXT_END,
+    TE_LINE_END,
+    TE_FONT_6x7,
+    TE_FONT_6x8,
+    TE_FONT_6x10,
+    TE_FONT_7x10,
+    TE_FONT_8x8,
+    TE_FONT_8x12,
+    TE_FONT_12x16,
+    TE_COLOUR_LIGHT_GREY,
+    TE_COLOUR_DARK_GREY,
+    TE_COLOUR_BLACK,
+    TE_SENT_MAX
+};
 
 void start_text_edit(void);
 void text_editor_open(const char *filename);
