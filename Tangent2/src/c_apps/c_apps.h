52c35ebd43a7cc9f21acacdd2e8dde3928f30d3d
u68 <78559138+u68@users.noreply.github.com>
2026-03-01 20:47:55 +1300

    head

---
+++ Tangent2/src/c_apps/c_apps.h
@@ -0,0 +1,16 @@
+/*
+ * c_apps.c
+ * Interface of interface of c_apps
+ *  Created on: March 1, 2026
+ *      Author: harma
+ */
+
+// Include all the headers for the C applications here
+#include "fs_man/fs_man.h"
+#include "media_viewer/media_view.h"
+#include "paint/paint.h"
+#include "text_editor/text_edit.h"
+#include "games/2048/2048.h"
+#include "games/ball/ball.h"
+#include "games/minesweeper/minesweeper.h"
+#include "games/snake/snake.h"
